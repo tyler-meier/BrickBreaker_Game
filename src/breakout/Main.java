78ff60d2af4773214dc62323d9dd20fa46b1adb7
Tyler Meier <tkm22@duke.edu>
2020-01-13 23:12:49 -0500

    started project, imported things i'll need

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -1,12 +1,21 @@
 package breakout;
 
-/**
- * Feel free to completely change this code or delete it entirely. 
- */
+import javafx.application.*;
+import javafx.animation.*;
+import javafx.scene.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.stage.*;
+import javafx.util.Duration;
+
+
 public class Main {
-    /**
-     * Start of the program.
-     */
+    public static final String PADDLE_IMAGE = "paddle.gif";
+
+
     public static void main (String[] args) {
         System.out.println("Hello world");
     }
