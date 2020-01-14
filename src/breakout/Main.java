19a4cda98883912df56fe0c8477585dcc7c16462
Tyler Meier <tkm22@duke.edu>
2020-01-14 17:47:52 -0500

    added stuff to main, added images, created classes

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -1,22 +1,43 @@
 package breakout;
 
-import javafx.application.*;
-import javafx.animation.*;
-import javafx.scene.*;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.*;
-import javafx.scene.paint.*;
-import javafx.scene.shape.*;
-import javafx.stage.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 
+
 public class Main {
-    public static final String PADDLE_IMAGE = "paddle.gif";
+    public static final String TITLE = "Example JavaFX";
+    public static final int SIZE = 400;
+    public static final int FRAMES_PER_SECOND = 60;
+    public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    public static final Paint BACKGROUND = Color.AZURE;
+    public static final Paint HIGHLIGHT = Color.OLIVEDRAB;
+    public static final String BOUNCER_IMAGE = "ball.gif";
+    public static final int BOUNCER_SPEED = 40;
+
+    private Scene myScene;
+    private ImageView myBouncer;
+    private ImageView myPaddle;
+    private ImageView myBlockOne;
+    private ImageView myBlockTwo;
+    private ImageView myBlockThree;
+    private ImageView myBlockFour;
+
 
 
     public static void main (String[] args) {
+
         System.out.println("Hello world");
     }
 }
